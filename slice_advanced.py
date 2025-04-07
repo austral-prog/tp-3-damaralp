34b337da46c61ae5c4b3ac9005cfa7b72482e736
damaralp <damaralpaz@mail.austral.edu.ar>
2025-04-07 12:23:45 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,7 @@
 def slice_advanced():
+    name=input("Ingrese texto aqui!:")
+    print(name[4::2])
+
     # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
